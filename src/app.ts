a7fb2d069a156bd84e74926e8f9de57b731b3aac
Nathalya Ohana Pires Ferreira <nathyohana@gmail.com>
2023-06-26 16:55:08 -0400

    feat: validation middleware created

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use((req, res, next) => {});
+app.use((req, res, next) => { });
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Server running on port: ${port}`));
